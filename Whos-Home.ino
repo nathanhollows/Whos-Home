a22f5f2cc6707994961866a7e4e48ed14508d5e1
markuz449 <markuz449@gmail.com>
2020-03-22 15:09:13 +1300

    Can now enter Access Point mode using flash button

--- Whos-Home.ino
+++ Whos-Home.ino
@@ -1,5 +1,6 @@
 #include "./esppl_functions.h"
 #include <time.h>
+#include <EasyButton.h>
 #include "Settings.h"
 
 uint8_t friendmac[DEVICEAMOUNT][ESPPL_MAC_LEN] = DEVICEMAC;
@@ -10,15 +11,71 @@ const byte ledPinList[DEVICEAMOUNT] = PINS;
 // Time to light up each LED in seconds
 // Optimises over time
 int delayTime[DEVICEAMOUNT] = {5, 5, 5, 5};
+int LEDState[DEVICEAMOUNT] = {0, 0, 0, 0};
+
+// Used for selection when NodeMCU is an access point
+EasyButton AccessButton(0);
+EasyButton DeviceButton(0);
+bool ACMode = false;
+int deviceSelect = 0;
 
 void setup() {
   delay(500);
   Serial.begin(115200);
   esppl_init(cb);
   for (int i = 0; i < DEVICEAMOUNT; i++){
-    lastSeen[i] = 0;
     pinMode(ledPinList[i], OUTPUT);
+    lastSeen[i] = 0;
   }
+  AccessButton.begin();
+  DeviceButton.begin();
+  AccessButton.onPressed(accessPointMode);
+  DeviceButton.onPressed(deviceReset);
+}
+
+void blinkLED(){
+  for (int i = 0; i < DEVICEAMOUNT; i++){
+    digitalWrite(ledPinList[i], HIGH);
+    delay(100);
+    digitalWrite(ledPinList[i], LOW);
+  }
+}
+
+void resetState(){
+  Serial.println("Resetting");
+  esppl_sniffing_start();
+  for (int i = 0; i < DEVICEAMOUNT; i++){
+    Serial.printf("Name: %s, Check: %d\n", friendname[i].c_str(), LEDState[i]);
+    if (LEDState[i] == 1){
+      digitalWrite(ledPinList[i], HIGH);
+      lastSeen[i] = millis() / 1000;
+    } else{
+      digitalWrite(ledPinList[i], LOW);
+    }
+  }
+}
+
+void deviceReset(){
+  Serial.println("Device Select");
+  digitalWrite(ledPinList[deviceSelect], LOW);
+  deviceSelect += 1;
+  if (deviceSelect >= DEVICEAMOUNT){
+    deviceSelect = 0;
+  }
+  digitalWrite(ledPinList[deviceSelect], HIGH);
+}
+
+void accessPointMode(){
+  Serial.println("Access Mode");
+  ACMode = true;
+  esppl_sniffing_stop();
+  blinkLED();
+  delay(100);
+  blinkLED();
+  for (int i = 0; i < DEVICEAMOUNT; i++){
+    digitalWrite(ledPinList[i], LOW);
+  }
+  digitalWrite(ledPinList[deviceSelect], HIGH);
 }
 
 bool maccmp(uint8_t *mac1, uint8_t *mac2) {
@@ -43,6 +100,7 @@ void printWhosHere(int person){
 
   lastSeen[person] = time_s / 1000;
   digitalWrite(ledPinList[person], HIGH);
+  LEDState[person] = 1;
 
   Serial.printf("%d\t%d\t%s\r\n", time_s, delayTime[person], friendname[person].c_str());
 }
@@ -58,15 +116,27 @@ void cb(esppl_frame_info *info) {
 void loop() {
   esppl_sniffing_start();
   while (true) {
-    for (int i = ESPPL_CHANNEL_MIN; i <= ESPPL_CHANNEL_MAX; i++ ) {
-      esppl_set_channel(i);
-      esppl_process_frames();
-    }
-    for (int i = 0; i < DEVICEAMOUNT; i++){
-      time_s = millis() / 1000;
-      if (time_s - lastSeen[i] > delayTime[i]){
-        digitalWrite(ledPinList[i], LOW);
+    while (ACMode == false){
+      for (int i = ESPPL_CHANNEL_MIN; i <= ESPPL_CHANNEL_MAX; i++ ) {
+        esppl_set_channel(i);
+        esppl_process_frames();
       }
+      for (int i = 0; i < DEVICEAMOUNT; i++){
+        time_s = millis() / 1000;
+        if (time_s - lastSeen[i] > delayTime[i]){
+          digitalWrite(ledPinList[i], LOW);
+          LEDState[i] = 0;
+        }
+      }
+      AccessButton.read();
+    }
+    unsigned int AccessTime = millis() / 1000;
+    while (AccessTime + 30 > (millis() / 1000)){
+      DeviceButton.read();
+      delay(100);
+    }
+    if (ACMode == true){
+      resetState();
     }
   }
 }
