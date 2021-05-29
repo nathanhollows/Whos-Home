39d7c7817cf8ae89effc1d6983d880a225bedcbd
markuz449 <markuz449@gmail.com>
2021-05-30 11:14:20 +1200

    Updated the code to work with the new Who's home device

--- Whos-Home.ino
+++ Whos-Home.ino
@@ -1,5 +1,4 @@
 #include <time.h>
-#include <EasyButton.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
@@ -14,32 +13,32 @@ ESP8266WebServer server(80);
 uint8_t friendmac[DEVICEAMOUNT][ESPPL_MAC_LEN];
 unsigned long time_s;
 unsigned long lastSeen[DEVICEAMOUNT];
-const byte ledPinList[DEVICEAMOUNT] = {D2, D1, D0, D5};
+const byte ledPinList[DEVICEAMOUNT] = {D7, D5, D4, D1};
 // Time to light up each LED in seconds
 // Optimises over time
 int delayTime[DEVICEAMOUNT] = {5, 5, 5, 5};
 int LEDState[DEVICEAMOUNT] = {0, 0, 0, 0};
 
 // Used for selection when NodeMCU is an access point
-EasyButton AccessButton(0);
-EasyButton DeviceButton(0);
+byte buttonPin = D8;
+int buttonState = 0;
 bool ACMode = false;
-int deviceSelect = 0;
+int currentSelectedDevice = 0;
 
 // Sets up the NodeMCU ready for the program
 void setup() {
   delay(500);
   Serial.begin(115200);
+  Serial.println("Starting...");
   esppl_init(cb);
   for (int i = 0; i < DEVICEAMOUNT; i++){
     pinMode(ledPinList[i], OUTPUT);
     lastSeen[i] = 0;
   }
-  AccessButton.begin();
-  DeviceButton.begin();
-  AccessButton.onPressed(accessPointMode);
-  DeviceButton.onPressed(deviceReset);
-
+  
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  
   // Gets the MAC addresses from the EEPROM storage
   EEPROM.begin(24);
   int addr = 0;
@@ -63,7 +62,7 @@ void accessPointMode(){
   for (int i = 0; i < DEVICEAMOUNT; i++){
     digitalWrite(ledPinList[i], LOW);
   }
-  digitalWrite(ledPinList[deviceSelect], HIGH);
+  digitalWrite(ledPinList[currentSelectedDevice], HIGH);
   wifi_promiscuous_enable(false);
   delay(200);
   Serial.print("Configuring access point...");
@@ -76,12 +75,14 @@ void accessPointMode(){
 // Blinks the LED's and lets the user know they can set their devices
 void blinkLED(bool accessMode){
   if (accessMode){
+    // Direction going down
     for (int i = 0; i < DEVICEAMOUNT; i++){
       digitalWrite(ledPinList[i], HIGH);
       delay(100);
       digitalWrite(ledPinList[i], LOW);
     }
   } else{
+    // Direction going up
     for (int i = DEVICEAMOUNT - 1; i > -1; i--){
       digitalWrite(ledPinList[i], HIGH);
       delay(100);
@@ -108,14 +109,14 @@ void resetState(){
 }
 
 // Let's the user see what device they are setting when in Access mode
-void deviceReset(){
+void deviceSelect(){
   Serial.println("Device Select");
-  digitalWrite(ledPinList[deviceSelect], LOW);
-  deviceSelect += 1;
-  if (deviceSelect >= DEVICEAMOUNT){
-    deviceSelect = 0;
+  digitalWrite(ledPinList[currentSelectedDevice], LOW);
+  currentSelectedDevice += 1;
+  if (currentSelectedDevice >= DEVICEAMOUNT){
+    currentSelectedDevice = 0;
   }
-  digitalWrite(ledPinList[deviceSelect], HIGH);
+  digitalWrite(ledPinList[currentSelectedDevice], HIGH);
 }
 
 // Checks the client connected to the Access point and stores their MAC at the selected LED
@@ -124,17 +125,17 @@ bool client_status() {
   stat_info = wifi_softap_get_station_info();
   
   if (stat_info != NULL) {
-    digitalWrite(ledPinList[deviceSelect], LOW);
+    digitalWrite(ledPinList[currentSelectedDevice], LOW);
     delay(200);
-    digitalWrite(ledPinList[deviceSelect], HIGH);
+    digitalWrite(ledPinList[currentSelectedDevice], HIGH);
     delay(200);
-    digitalWrite(ledPinList[deviceSelect], LOW);
+    digitalWrite(ledPinList[currentSelectedDevice], LOW);
     delay(200);
-    digitalWrite(ledPinList[deviceSelect], HIGH);
-    int storeAddr = deviceSelect * 6;
+    digitalWrite(ledPinList[currentSelectedDevice], HIGH);
+    int storeAddr = currentSelectedDevice * 6;
     for (int i = 0; i < 6; i++){
-      friendmac[deviceSelect][i] = stat_info->bssid[i];
-      EEPROM.put((storeAddr + i), friendmac[deviceSelect][i]);
+      friendmac[currentSelectedDevice][i] = stat_info->bssid[i];
+      EEPROM.put((storeAddr + i), friendmac[currentSelectedDevice][i]);
       EEPROM.commit(); 
     }
     stat_info = STAILQ_NEXT(stat_info, next);
@@ -181,13 +182,54 @@ void cb(esppl_frame_info *info) {
 }
 
 // Main program loop that checks if people are here and if the FLASH button is pressed
+//void loop() {
+//  esppl_sniffing_start();
+//  while (true) {
+//    Serial.println("Looping...");
+//    while (ACMode == false){
+//      for (int i = ESPPL_CHANNEL_MIN; i <= ESPPL_CHANNEL_MAX; i++ ) {
+//        esppl_set_channel(i);
+//        esppl_process_frames();
+//      }
+//      for (int i = 0; i < DEVICEAMOUNT; i++){
+//        time_s = millis() / 1000;
+//        if (time_s - lastSeen[i] > delayTime[i]){
+//          digitalWrite(ledPinList[i], LOW);
+//          LEDState[i] = 0;
+//        }
+//      }
+//      AccessButton.read();
+//    }
+//    unsigned int AccessTime = millis() / 1000;
+//    while (AccessTime + 30 > (millis() / 1000)){
+//      DeviceButton.read();
+//      server.handleClient();
+//      if (client_status()){
+//        break;
+//      }
+//      delay(100);
+//    }
+//    if (ACMode == true){
+//      blinkLED(false);
+//      delay(100);
+//      blinkLED(false);
+//      resetState();
+//      ACMode = false;
+//    }
+//  }
+//}
+
 void loop() {
   esppl_sniffing_start();
   while (true) {
+    // Looping main functions while not in access mode
     while (ACMode == false){
+      delay(100);
+
+      // Handles the promiscuous mode checking
       for (int i = ESPPL_CHANNEL_MIN; i <= ESPPL_CHANNEL_MAX; i++ ) {
         esppl_set_channel(i);
-        esppl_process_frames();
+        //esppl_process_frames();
       }
       for (int i = 0; i < DEVICEAMOUNT; i++){
         time_s = millis() / 1000;
@@ -196,17 +238,34 @@ void loop() {
           LEDState[i] = 0;
         }
       }
-      AccessButton.read();
+      // Checks if the button is pressed, if so changes to Access Mode
+      buttonState = digitalRead(buttonPin);
+      if (buttonState == HIGH){
+        Serial.println("Changing to access mode");
+        accessPointMode();
+        //yield();
+      }
     }
+
+    // Sets a timer of 30 seconds for access mode to last
+    // Handles the client connections to the access point
     unsigned int AccessTime = millis() / 1000;
     while (AccessTime + 30 > (millis() / 1000)){
-      DeviceButton.read();
       server.handleClient();
       if (client_status()){
         break;
       }
+      // Changes the current selected device
+      buttonState = digitalRead(buttonPin);
+      if (buttonState == HIGH){
+        Serial.println("Changing selected device");
+        deviceSelect();
+        yield();
+      }
       delay(100);
     }
+
+    // Checks if the NodeMCU is in access mode, if so it changes back to promiscuous mode
     if (ACMode == true){
       blinkLED(false);
       delay(100);
